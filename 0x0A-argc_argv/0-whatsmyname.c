34698a8ca2e7471fbad13323c8e50936119938c6
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-10-27 08:42:16 -0700

    Low-level_language

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,59 +1,16 @@
-#include <stdio.h>
-
-                
-
-#include "main.h"
-
-                
-
-
-                
-
-/**
-
-                
-
- * main - prints the name of the program
-
-                
-
- * @argc: number of arguments
-
-                
-
- * @argv: array of arguments
-
-                
-
- *
-
-                
-
- * Return: Always 0 (Success)
-
-                
-
- */
-
-                
-
-int main(int argc __attribute__((unused)), char *argv[])
-
-                
-
-{
-
-                
-
-        printf("%s\n", *argv);
-
-                
-
-
-                
-
-        return (0);
-
-                
-
-}
+#include <stdio.h> 
+ #include <stdlib.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * @argc: Counts the number of parameters that go into main 
+  * @argv: Pointer of array of pointers containing strings entering main 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(int argc, char **argv) 
+ { 
+         if (argc > 0) 
+                 printf("%s\n", argv[0]); 
+         return (0); 
+ }
