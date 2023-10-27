57dbfb8e548b0bd7abacf647972b303e52cc3b73
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-10-27 10:11:09 -0700

    Low-level_program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,17 @@
 #include <stdio.h> 
- #include <stdlib.h> 
   
  /** 
-  * main - Entry point 
+  * main - program that prints the number of arguments passed into it 
+  * @argc: number of arguments 
+  * @argv: array with the arguments 
   * 
-  * @argc: Counts the number of parameters that go into main 
-  * @argv: Pointer of array of pointers containing strings entering main 
-  * Return: Always 0 (Success) 
-  */ 
- int main(int argc, char **argv) 
+  * Return: always 0 
+  **/ 
+  
+ int main(int argc, char *argv[]) 
  { 
-         (void) argv; 
-         printf("%i\n", argc - 1); 
+         (void)argv; 
+  
+         printf("%d\n", argc - 1); 
          return (0); 
  }
