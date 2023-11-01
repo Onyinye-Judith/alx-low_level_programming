8f87800b0358c3caed6ceb076bf77cb7fb936392
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-10-31 21:01:14 -0700

    Malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdlib.h>
 
 /**
@@ -7,9 +8,7 @@
  * @c: char to assign
  * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *str;
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,40 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
-  * _strlen - return the lenth of the string as an int value
-  * @s: the string to measure.
-  *
-  * Return: Int value of the lenth.
-  */
-int _strlen(char *s)
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ *
+ */
+char *str_concat(char *s1, char *s2)
 {
-	int count = 0;
-	if(!s)
-		return (0);
-
-	while (*(s + count) != '\0')
+	char *conct;
+	int i, ci;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+	if (conct == NULL);
+	return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		count++;
-	}
-	return (count);
+		conct[i] = s1[i];
+		i++;
+        }
+	while (s2[ci] != '\0')
+        {
+		conct[i] = s2[ci]
+			i++, ci++;
+        }
+	conct[i] = '\0';
+	return (conct);
 }
-
-
