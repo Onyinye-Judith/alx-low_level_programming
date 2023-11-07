9dcaedd74126fc7225ed931f7f8755a15377d230
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-11-06 19:10:24 -0800

    New task

--- dog.h
+++ dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - list dates about pet
- * @name: pet name
- * @age: pet age
- * @owner: pet owner name
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
  */
 struct dog
 {
@@ -12,12 +13,14 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 /**
- * dog - rename strucut with typedef
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d); 
-#endif /* DOG_H */
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
