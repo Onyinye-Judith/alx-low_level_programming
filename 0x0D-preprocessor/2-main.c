3c1db011535a7f605f8684d6bf97b43ed0221089
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-11-08 18:47:34 -0800

    0-print_name.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,13 +2,18 @@
 
 /**
  * main - prints the name of the file
+
+ *
+
  * Return: Always 0 (Success)
+
  */
 
 int ain(void)
 
 {
-	printf("%s\n", __FILE__);
+
+        printf("%s\n", __FILE__);
 
         return (0);
 
