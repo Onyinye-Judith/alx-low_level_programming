968dc2806d81d53805b6471e4b0ff6fd2ed55a5a
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-11-20 11:28:14 -0800

    More-malloc

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,46 +1,20 @@
+#include <stddef.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: Pointer to the head of the list
- * @idx: Index of the list where the new node should be added
- * @n: Value to be assigned to the new node
- * Return: The address of the new node, or NULL if it failed
+ * listint_len - Counts the number of elements in a listint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+size_t listint_len(const listint_t *h)
 {
-	listint_t *new_node, *current;
-	unsigned int i;
-
-	if (head == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (idx == 0)
+	size_t count = 0;
+	while (h != NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		count++;
+		h = h->next;
 	}
 
-	current = *head;
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	return (count);
 }
