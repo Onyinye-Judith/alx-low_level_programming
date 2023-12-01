4eb067ae7129e65e119e7c298e3d5f25e4dc2a8b
Onyinye-Judith <onyinyejudith55@yahoo.com>
2023-12-01 00:31:10 -0800

    its elf

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,47 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- * Return: The actual number of letters it could read and print.
+ * Return: The actual number of letters read and printed. 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor, bytes_read, bytes_written;
+	int fd, read_chars, write_chars;
 	char *buffer;
-
 	if (filename == NULL)
-		return (0);
-
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(file_descriptor);
 		return (0);
-	}
 
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		free(buffer);
-		close(file_descriptor);
 		return (0);
 	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	read_chars = read(fd, buffer, letters);
+	if (read_chars == -1)
 	{
 		free(buffer);
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
-
+	
+	write_chars = write(STDOUT_FILENO, buffer, read_chars);
+	if (write_chars == -1 || write_chars != read_chars)
+	{
+		free(buffer)
+		close(fd)			return (0);
+	}	
 	free(buffer);
-	close(file_descriptor);
-
-	return (bytes_written);
+	close(fd);
+	return (read_chars);
 }
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,94 +1,69 @@
-#include <stdio.h>
+ <stdio.h>
 #include <stdlib.h>
-#include <elf.h>
+#include <stdint.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
 
-/**
- * error_exit - Prints an error message to stderr and exits with a status code.
- * @msg: The error message to print.
- * @exit_code: The exit status code.
- */
-void error_exit(char *msg, int exit_code)
-{
-	dprintf(2, "%s\n", msg);
-	exit(exit_code);
+void print_elf_header(const Elf64_Ehdr *header) {
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; ++i)
+        printf(" %02x", header->e_ident[i]);
+    
+    printf("\n  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
+    printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
 }
 
-/**
- * print_elf_header - Displays the information contained in the ELF header.
- * @header: Pointer to the ELF header structure.
- */
-void print_elf_header(Elf64_Ehdr *header)
-{
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-		printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(98);
+    }
 
-	printf("  Class:                             %s\n",
-	       header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n",
-	       header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            ");
-	switch (header->e_ident[EI_OSABI])
-	{
-	case ELFOSABI_NONE:
-		printf("UNIX - System V\n");
-		break;
-	case ELFOSABI_LINUX:
-		printf("UNIX - Linux\n");
-		break;
-	case ELFOSABI_NETBSD:
-		printf("UNIX - NetBSD\n");
-		break;
-	case ELFOSABI_SOLARIS:
-		printf("UNIX - Solaris\n");
-		break;
-	default:
-		printf("<unknown: %d>\n", header->e_ident[EI_OSABI]);
-		break;
-	}
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(98);
+    }
+int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(98);
+    }
 
-	/* Add more fields as needed */
+    Elf64_Ehdr header;
 
-	printf("  Type:                              %s\n",
-	       header->e_type == ET_EXEC ? "EXEC (Executable file)" : "DYN (Shared object file)");
-	printf("  Entry point address:               %#lx\n", header->e_entry);
+    if (lseek(fd, 0, SEEK_SET) == -1) {
+        perror("Error seeking in file");
+        close(fd);
+        exit(98);
+    }
 
-	/* Add more fields as needed */
-}
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        exit(98);
+    }
 
-/**
- * main - Displays the information contained in the ELF header at the start of an ELF file.
- * @ac: The number of arguments.
- * @av: The array of arguments.
- * Return: 0 on success, 98 on error.
- */
-int main(int ac, char **av)
-{
-	int fd;
-	Elf64_Ehdr header;
+    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Error: Not an ELF file\n");
+        close(fd);
+        exit(98);
+    }
 
-	if (ac != 2)
-		error_exit("Usage: elf_header elf_filename", 97);
+    print_elf_header(&header);
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-		error_exit("Error: Can't open file", 98);
-
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-		error_exit("Error: Can't read from file", 98);
-
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-	    header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
-		error_exit("Error: Not an ELF file", 98);
-
-	print_elf_header(&header);
-
-	close(fd);
-	return (0);
+    close(fd);
+    return 0;
 }
