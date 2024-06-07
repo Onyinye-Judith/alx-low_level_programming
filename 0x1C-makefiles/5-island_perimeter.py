44ec098591e51aff445982ac7faa6e669d7eb2e0
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-06-07 05:02:37 +0100

    Make-files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""This module defines the function `island_perimeter`"""
+
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                    if j == 0 or grid[i][j - 1] != 1:
-                        c += 1
-                        if j == width or grid[i][j + 1] != 1:
-                            c += 1
-                            if i == length or grid[i + 1][j] != 1:
-                                c += 1
-                                return c
+    col_size = len(grid[0])
+    row_size = len(grid)
+    perimeter = 0
+
+    for row in range(row_size):
+        for col in range(col_size):
+            if grid[row][col]:
+                # check left-border
+                if col - 1 < 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                    # check right-border
+                    if col + 1 == col_size or grid[row][col + 1] == 0:
+                        perimeter += 1
+                        # check top-border
+                        if row - 1 < 0 or grid[row - 1][col] == 0:
+                            perimeter += 1
+                            # check bottom-border
+                            if row + 1 == row_size or grid[row + 1][col] == 0:
+                                perimeter += 1
+                                return perimeter
